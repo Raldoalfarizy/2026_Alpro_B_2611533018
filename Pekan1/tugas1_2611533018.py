ba3d9cfd3e31c43433b22a50fab4230f9e587eb1
Raldo Restanggi Alfarizy <Raldoalfarizy@gmail.com>
2026-09-10 14:46:25 +0800

    Praktikum pertemuan ke-1

---
+++ Pekan1/tugas1_2611533018.py
@@ -0,0 +1,14 @@
+nama = "Raldo Restanggi Alfarizy"
+nim = "2611533018"
+jurusan = "Informatika"
+universitas = "Universitas Andalas"
+angkatan = "2026"
+domisili = "Padang"
+
+print("=== BIODATA DIRI ===")
+print("Nama        :", nama)
+print("NIM         :", nim)
+print("Jurusan     :", jurusan)
+print("Universitas :", universitas)
+print("Angkatan    :", angkatan)
+print("Domisili    :", domisili)
