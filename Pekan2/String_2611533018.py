22da512d032a91083f2e933019426789079d34a3
Raldo Restanggi Alfarizy <Raldoalfarizy@gmail.com>
2026-09-10 10:59:33 +0800

    Praktikum Pertemuan ke-2

---
+++ Pekan2/String_2611533018.py
@@ -0,0 +1,15 @@
+# Buat file dengan nama String_NIM.py
+# Nama variabel ditambah 4 digit terakhir NIM contoh : a_3018
+# ganti programnya dan data masing masing
+nama_3018 = "Raldo Restanggi Alfarizy"
+jenis_kelamin_3018 = "Laki-laki"
+alamat_3018 = """
+    Kampus Unand
+    Kec. Pauh
+    Kota Padang
+"""
+agama_3018 = "Islam"
+print("Nama saya adalah", nama_3018)
+print("Jenis kelamin saya adalah", jenis_kelamin_3018)
+print("Alamat saya adalah", alamat_3018)
+print("Agama saya adalah", agama_3018)
